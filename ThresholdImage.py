f873cc601e18d7727b2c7a76730c58b6e9b0b653
yair <aythb@hotmail.com>
2021-12-09 17:24:19 -0600

    se agrega lineas de codigo para el threshold tozero tozero inv

--- ThresholdImage.py
+++ ThresholdImage.py
@@ -11,8 +11,13 @@ _, binarizada = cv2.threshold(imagen, 210,255,cv2.THRESH_BINARY)
 _, binarizada_inv = cv2.threshold(imagen, 210, 255,cv2.THRESH_BINARY_INV)
 #truncate
 _, trunc = cv2.threshold(imagen, 210, 255, cv2.THRESH_TRUNC)
+#tozero 
+_, tozero = cv2.threshold(imagen, 210, 255, cv2.THRESH_TOZERO)
+#Tozero inv
+_, tozero_inv =cv2.threshold(imagen, 210,255, cv2.THRESH_TOZERO_INV)
 
 cv2.imshow('IMAGEN DE PRUEBA', imagen)
 cv2.imshow('TIPOS: Binary - Binary-inv - Truncate', np.hstack([binarizada, binarizada_inv, trunc]))
+cv2.imshow('TIPOS: TO ZERO - TO ZERO-INV', np.hstack([tozero, tozero_inv]))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
